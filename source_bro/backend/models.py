125b088759b0b4b4b88157c41f3c8457bbc370f8
Zane Kissel <zekissel@gmail.com>
2023-04-16 01:43:45 -0400

    add serialize and doc

--- source_bro/backend/models.py
+++ source_bro/backend/models.py
@@ -1,10 +1,13 @@
 import enum
 from flask_sqlalchemy import SQLAlchemy
 
+# database object
 db = SQLAlchemy()
 
+
+# ---------------- DATABASE MODELS
 class User(db.Model):
-    user_id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     user_name = db.Column(db.String(32), nullable=False)
     pw_hash = db.Column(db.String(64), nullable=False)
     user_email = db.Column(db.String(64), nullable=True)    # users can register without an email
@@ -12,47 +15,56 @@ class User(db.Model):
     accounts = db.relationship('Account', backref='owner', lazy='dynamic')
     shares = db.relationship('Share', backref='owner', lazy='dynamic')
 
-    def __init__(self, uid, name, pw):
-        self.user_id = uid
+    def __init__(self, name, pw):
         self.user_name = name
         self.pw_hash = pw
 
+    def serialize(self):
+        return {'id': self.user_id, 'name': self.user_name}
+
+    # debug
     def __repr__(self):
         return '<User {} - {}>'.format(self.user_id, self.user_name)
     
 
 class Account(db.Model):
-    acc_id = db.Column(db.Integer, primary_key=True)
+    acc_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     acc_name = db.Column(db.String(32), nullable=False)
-    acc_age = db.Column(db.Integer, nullable=False)
+    acc_age = db.Column(db.Integer, nullable=False)     # time since account creation
     acc_total = db.Column(db.Double, nullable=False)
 
     acc_holder = db.Column(db.Integer, db.ForeignKey('user.user_id'))
 
-    def __init__(self, aid, name, total):
-        self.acc_id = aid
+    def __init__(self, name, total):
         self.acc_name = name
         self.acc_age = 0
         self.acc_total = total
 
+    def serialize(self):
+        return {'id': self.acc_id, 'name': self.acc_name, 'age': self.acc_age}
+
     def __repr__(self):
         return '<Account {} - ${}>'.format(self.acc_id, self.acc_total)
+
 
 class Share(db.Model):
     stock_id = db.Column(db.String(5), primary_key=True)
     stock_name = db.Column(db.String(32), nullable=False)
-    stock_value = db.Column(db.Double, nullable=False)
-    num_shares = db.Column(db.Double, nullable=False)
-    avg_price = db.Column(db.Double, nullable=False)
+    stock_value = db.Column(db.Double, nullable=False) # current stock price
+    num_shares = db.Column(db.Double, nullable=False)  # current shares held
+    avg_price = db.Column(db.Double, nullable=False)   # price of stock at different purchase times (if > 1), weighted by num_shares purchased
 
     share_holder = db.Column(db.Integer, db.ForeignKey('user.user_id'))
 
     def __init__(self, sid, name, val, num, avg):
-        self.stock_id = aid
+        self.stock_id = sid
         self.stock_name = name
         self.stock_value = val
         self.num_shares = num
         self.avg_price = avg
+
+    def serialize(self):
+        return {'id': self.stock_id, 'name': self.stock_name, 'value': self.stock_value}
 
     def __repr__(self):
         return '<Stock {} - {} shares @ ${} - Current ${}>'.\
@@ -66,20 +78,24 @@ class Unit(enum.Enum):
     week = 4
 
 class Task(db.Model):
-    task_id = db.Column(db.Integer, primary_key=True)
+    task_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     task_name = db.Column(db.String(32), nullable=False)
-    task_is_expense = db.Column(db.Boolean, nullable=False)     # if False, task is projected income
+    task_is_expense = db.Column(db.Boolean, nullable=False)     # if False, task is projected income (expense if True)
     task_amount = db.Column(db.Double, nullable=False)
 
-    notify = db.Column(db.Boolean, nullable=True)
+    # notification settings for scheduled events
+    notify = db.Column(db.Boolean, nullable=False)
     notify_unit = db.Column(db.Enum(Unit), nullable=True)
     notify_length = db.Column(db.Double, nullable=True)
 
-    def __init__(self, tid, name, exp, amount):
-        self.task_id = tid
+    def __init__(self, name, exp, amount, notify):
         self.task_name = name
         self.task_is_expense = exp
         self.task_amount = amount
+        self.notify = notify
+
+    def serialize(self):
+        return {'id': self.task_id, 'name': self.task_name}
 
     def __repr__(self):
         return '<Task {} - {}>'.format(self.task_id, self.task_name)
