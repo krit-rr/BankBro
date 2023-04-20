a2f3e99657ef1591ff232516b841bdde83f88763
EESHIN <52087813+EESHIN@users.noreply.github.com>
2023-04-20 02:03:25 -0400

    styling and code for spinner component

--- client/src/components/Spinner/index.js
+++ client/src/components/Spinner/index.js
@@ -1,1 +1,14 @@
+import { ImSpinner2 } from "react-icons/im";
+import "./style.css";
 
+const Spinner = () => {
+  return (
+    <>
+      <div className="spinner-container">
+        <ImSpinner2 className="spinner" size="50px" />
+      </div>
+    </>
+  );
+};
+
+export default Spinner;
