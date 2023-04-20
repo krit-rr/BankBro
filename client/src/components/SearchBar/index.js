390a273ea82f244b322f56ec1ccb49898c66f32e
EESHIN <52087813+EESHIN@users.noreply.github.com>
2023-04-20 02:04:39 -0400

    styling and code for search bar

--- client/src/components/SearchBar/index.js
+++ client/src/components/SearchBar/index.js
@@ -1,1 +1,47 @@
+import { useState } from "react";
+import { FaSearch } from "react-icons/fa";
 
+import useWindowPosition from "../../hooks/useWindowPosition";
+import "./style.css";
+
+const Searchbar = ({ searchSubmit }) => {
+  const scrollPosition = useWindowPosition();
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    searchSubmit(searchTerm);
+    setSearchTerm("");
+  };
+
+  return (
+    <header
+      id="header"
+      className={`${scrollPosition > 25 ? "header-sticky" : ""}`}
+    >
+      <div className="header__inner">
+        <form action="" onSubmit={(e) => handleSubmit(e)}>
+          <div>
+            <input
+              type="text"
+              placeholder="Search by ticker symbol..."
+              title=""
+              value={searchTerm}
+              onChange={(e) => setSearchTerm(e.target.value.toUpperCase())}
+              required
+            />
+            <button
+              type="submit"
+              className="search-btn"
+              aria-label="Submit search"
+            >
+              <FaSearch className="search-icon" />
+            </button>
+          </div>
+        </form>
+      </div>
+    </header>
+  );
+};
+
+export default Searchbar;
