770b5b746ffa999972a187a5d157617aecf511a5
EESHIN <52087813+EESHIN@users.noreply.github.com>
2023-04-20 02:14:36 -0400

    handle immediate stock data like price

--- client/src/components/Content/Quote/index.js
+++ client/src/components/Content/Quote/index.js
@@ -1,1 +1,28 @@
+import { unixToDateString } from "../../../utils/date";
+import { formatChangeString } from "../../../utils/format";
+import "./style.css";
 
+const Quote = ({ data }) => {
+  return (
+    <section id="quote-section">
+      <h2 className="quote">
+        {data.latestPrice || "--"}
+        <span
+          className={`${
+            Number(data.change) >= 0 ? "color-bullish" : "color-bearish"
+          }`}
+        >
+          {" "}
+          {formatChangeString(data.change, data.changePercent) || "-- (--)"}
+        </span>
+      </h2>
+      <p className="quote-meta">
+        {data.latestSource || "--"} |{" "}
+        {unixToDateString(data.latestUpdate) || "--"}
+        
+      </p>
+    </section>
+  );
+};
+
+export default Quote;
