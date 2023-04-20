37b49028c74b97850ff3acc58d7d752785fb1382
EESHIN <52087813+EESHIN@users.noreply.github.com>
2023-04-20 01:59:55 -0400

    Add files via upload

--- client/src/hooks/useAppState.js
+++ client/src/hooks/useAppState.js
@@ -1,1 +1,37 @@
+import { useState, useEffect } from "react";
+import fetchSymbolData from "../utils/api";
 
+const useAppState = () => {
+  const [symbolData, setSymbolData] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    // Default to searching for TSLA on initial page load
+    const initialSearch = async () => {
+      const data = await fetchSymbolData("TSLA");
+      setSymbolData(data);
+      setIsLoading(false);
+    };
+    initialSearch();
+  }, []);
+
+  useEffect(() => {
+    // Scroll to top of the page after every search
+    window.scrollTo(0, 0);
+  }, [symbolData]);
+
+  const handleSearch = async (query) => {
+    setIsLoading(true);
+    const data = await fetchSymbolData(query);
+    setSymbolData(data);
+    setIsLoading(false);
+  };
+
+  return {
+    symbolData,
+    isLoading,
+    handleSearch,
+  };
+};
+
+export default useAppState;
---
+++ client/src/hooks/useWindowPosition.js
@@ -0,0 +1,19 @@
+import { useState, useEffect } from "react";
+
+const useWindowPosition = () => {
+  const [scrollPosition, setScrollPosition] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const position = window.pageYOffset;
+      setScrollPosition(position);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  return scrollPosition;
+};
+
+export default useWindowPosition;
