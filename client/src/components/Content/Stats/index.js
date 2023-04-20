567cf3bdd1f85eae392b3842c6dedf986557d7d7
EESHIN <52087813+EESHIN@users.noreply.github.com>
2023-04-20 02:15:34 -0400

    Stats for things like highest price of the year, and volume

--- client/src/components/Content/Stats/index.js
+++ client/src/components/Content/Stats/index.js
@@ -1,1 +1,57 @@
+import { formatLargeNumbers } from "../../../utils/format";
+import "./style.css";
 
+const Stats = ({ data }) => {
+  return (
+    <section id="stats-section">
+      <div className="stats-content">
+        <div className="stats-grid">
+          <div className="column">
+          </div>
+          <div className="column">
+            <div>
+              <p>Prev. Close</p>
+              <p>{data.previousClose || "--"}</p>
+            </div>
+            <div>
+              <p>Open</p>
+              <p>{data.open || "--"}</p>
+            </div>
+          </div>
+          <div className="column">
+            <div>
+              <p>High</p>
+              <p>{data.high || "--"}</p>
+            </div>
+            <div>
+              <p>Low</p>
+              <p>{data.low || "--"}</p>
+            </div>
+          </div>
+          <div className="column">
+            <div>
+              <p>52 Week High</p>
+              <p>{data.week52High || "--"}</p>
+            </div>
+            <div>
+              <p>52 Week Low</p>
+              <p>{data.week52Low || "--"}</p>
+            </div>
+          </div>
+          <div className="column">
+            <div>
+              <p>Volume</p>
+              <p>{formatLargeNumbers(data.latestVolume) || "--"}</p>
+            </div>
+            <div>
+              <p>Avg. Volume</p>
+              <p>{formatLargeNumbers(data.avgTotalVolume) || "--"}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Stats;
